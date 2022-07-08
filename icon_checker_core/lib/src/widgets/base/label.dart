de2471fe0609bd2c35b5d47a3c14a7f6f938408a
Florian Blasius <florian.blasius@ergosign.de>
2022-07-08 13:13:32 +0200

    add lable widget

---
+++ icon_checker_core/lib/src/widgets/base/label.dart
@@ -0,0 +1,52 @@
+import 'package:icon_checker_core/icon_checker_core.dart';
+import 'package:flutter/widgets.dart';
+
+enum TextLevel { pill, title1 }
+
+class Label extends StatelessWidget {
+  const Label.pill(
+    this.data, {
+    Key? key,
+    this.color,
+    this.fontSize,
+    this.maxLines,
+  })  : level = TextLevel.pill,
+        super(key: key);
+
+  const Label.title1(
+    this.data, {
+    Key? key,
+    this.color,
+    this.fontSize,
+    this.maxLines,
+  })  : level = TextLevel.title1,
+        super(key: key);
+
+  final String data;
+  final TextLevel level;
+  final Color? color;
+  final double? fontSize;
+  final int? maxLines;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = XTheme.of(context);
+    final color = this.color ?? theme.colors.labels;
+    final style = () {
+      switch (level) {
+        case TextLevel.pill:
+          return theme.typography.textStyle;
+        case TextLevel.title1:
+          return theme.typography.textStyle;
+      }
+    }();
+    return Text(
+      data,
+      style: style.copyWith(
+        color: color,
+        fontSize: fontSize,
+      ),
+      maxLines: maxLines,
+    );
+  }
+}
